817d47f1ecd70a5184bbcdbdbaaf02cb33bda2c4
Ricardo Branco <28.ricardobranco@gmail.com>
2013-04-27 22:51:25 +0100

    li4-anuncio

---
+++ cv2job/cv2job/Models/Anuncio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cv2job.Models
+{
+    public class Anuncio
+    {
+        public int ID { get; set; }
+        public String Titulo { get; set; }
+        public String Descricao { get; set; }
+        public String InfoAdiconal { get; set; }
+        public String TipoEmprego { get; set; }
+        public bool eRenumerado { get; set; }
+        public decimal Renumeracao { get; set; }
+
+    }
+} 
---
+++ cv2job/cv2job/Models/Cv2Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace cv2job.Models
+{
+    public class Cv2Job
+    {
+    }
+    public class Cv2JobDBContext : DbContext
+    {
+        public DbSet<Anuncio> Anuncios  { get; set; } 
+    }
+}
