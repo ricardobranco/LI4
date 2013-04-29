88c4b569071e5bd8ff9252058513a1195444c916
lccaseiro58 <lccaseiro58@gmail.com>
2013-04-29 11:24:51 +0100

    utilizador

---
+++ cv2job/cv2job/Models/Morada.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cv2job.Models
+{
+    public class Morada
+    {
+        public String numero { get; set; }
+        public String nomeRua { get; set; }
+        public String localidade { get; set; }
+        public String codPost { get; set; }
+        public String distrito { get; set; }
+        public String pais { get; set; }
+    }
+}
---
+++ cv2job/cv2job/Models/Utilizador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cv2job.Models
+{
+    abstract class Utilizador
+    {
+        public String username { get; set; }
+        public String nome { get; set; }
+        public Morada morada { get; set; }
+        public String telefone { get; set; }
+        public String email { get; set; }
+        public Byte[] foto { get; set; }
+    }
+}
