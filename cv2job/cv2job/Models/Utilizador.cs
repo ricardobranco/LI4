3c6e35c913b97a77884c10058be45c0ceba99d21
lccaseiro58 <lccaseiro58@gmail.com>
2013-05-22 19:57:13 +0100

    improve utilizador

--- cv2job/cv2job/Models/Utilizador.cs
+++ cv2job/cv2job/Models/Utilizador.cs
@@ -8,13 +8,17 @@ namespace cv2job.Models
 {
     public class Utilizador
     {
-        public String username { get; set; }
-        public String nome { get; set; }
-        public Morada morada { get; set; }
-        public String telefone { get; set; }
-        public String email { get; set; }
-        public String dataNascFund { get; set; }   // data de nascimento para candidato/empregador, data fundação para empregadores empresa.
-        public Byte[] foto { get; set; } //E MELHOR POR O PATH<string> é mais eficiente
+        public int ID { get; set; }
+        public String Username { get; set; }
+        private String Password;
+        public String Nome { get; set; }
+        public Morada Morad { get; set; }
+        public String Nacionalidade { get; set; }
+        public String Telefone { get; set; }
+        public String Email { get; set; }
+        public DateTime DataNascFund { get; set; }
+        public DateTime DataReg { get; set; }
+        public String Foto { get; set; } //E MELHOR POR O PATH<string> é mais eficiente
 
     }
 }
