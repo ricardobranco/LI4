20c4607bedf547badd5606c95d27c1c261bf9128
Pedro Maia <maiarib@gmail.com>
2013-05-22 19:55:11 +0100

    Classes CInfoPessoal E Eprofissional
    Classes que vão auxiliar candidato para a criação do curriculo vitall

---
+++ cv2job/cv2job/Models/CInfoPessoal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cv2job.Models
+{
+    public class CInfoPessoal
+    {
+        public int ID { get; set; }
+        public String Sexo{get; set;}
+        public String Proficao{ get; set; }
+        public String EmpregoPretendido{ get; set; }
+        public String EstudaEM { get; set; }
+    }
+}
+
---
+++ cv2job/cv2job/Models/Eprofissional.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cv2job.Models
+{
+    public class Eprofissional : Controller
+    {
+        //
+        // GET: /Eprofissional/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
+
