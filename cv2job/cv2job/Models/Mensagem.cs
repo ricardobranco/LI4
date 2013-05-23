cc291dc84523ae8468472de1fbb6462b6077bd5a
Diogo Filipe Araújo Alves <a61030.lei.um@gmail.com>
2013-05-23 17:12:42 +0100

    Classe Mensagem

---
+++ cv2job/cv2job/Models/Mensagem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cv2job.Models
+{
+    public class Mensagem
+    {
+        public int ID { get; set; }
+        public String Emissor { get; set; }
+        public String Receptor { get; set; }
+        public String Assunto { get; set; }
+        public String CorpoMsg { get; set; }
+        public DateTime DataEnvio { get; set; }
+        public bool Lida { get; set; }
+
+    }
+
+}
