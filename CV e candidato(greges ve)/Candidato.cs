8f526dec027345d41a702a68850756614a310769
Pedro Maia <maiarib@gmail.com>
2013-06-01 23:54:43 +0100

    Cvgerator
    Pasta criada à parte.
    Diz-me o que tenho de fazer para juntar?

---
+++ CV e candidato(greges ve)/Candidato.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Candidato
+    {
+		public String username{get;set;};
+        public InfoPessoal pessoal { get; set; }
+        public InfoExtra info { get; set; }
+
+
+        public Candidato(String nameuser) {
+			this.username=nameuser;
+            this.pessoal = new InfoPessoal();
+            this.info = new InfoExtra();
+        }
+    }
+}
